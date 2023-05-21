fe42385bd619364edec7f4912190c398ae2d29e8
Victor Ribeiro <devjvribeiro@gmail.com>
2023-05-21 02:36:58 -0300

    feat: Link component

---
+++ app/vue/shared/components/Link.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router'
+
+  const props = defineProps<{
+    to?: string
+    href?: string
+  }>()
+</script>
+
+<template>
+  <RouterLink v-if="props.to && !props.href" :to="props.to">
+    <slot />
+  </RouterLink>
+
+  <RouterLink v-else-if="props.href && !props.to" :to="props.href">
+    <slot />
+  </RouterLink>
+
+  <RouterLink v-else-if="props.href && props.to" :to="props.to">
+    <slot />
+  </RouterLink>
+
+  <a v-else>
+    <slot />
+  </a>
+</template>
