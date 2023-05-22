05503b3a795e0f14895207585e4ace0513f90a04
Victor Ribeiro <devjvribeiro@gmail.com>
2023-05-21 23:56:50 -0300

    chore: namings

--- app/larapress/wp/composables/useWP.ts
+++ app/larapress/wp/composables/useWP.ts
@@ -2,39 +2,49 @@ import type { IWPPage, IWPPost } from '../types/wordpress'
 import type { IPages, IPost } from '../types/larapress'
 
 export function useWP() {
+  /**
+   * Fetches a single page from the WP REST API
+   * @param slug - the slug of the page to fetch
+   * @returns page - the page object
+   * @example
+   * ```ts
+   * const page = useWP().page('about')
+   * ```
+   */
   const page = async (slug: string): Promise<IPages> => {
     const response = await fetch(`/wp-json/wp/v2/pages?slug=${slug}`)
     const data = await response.json()
 
-    const apiDataTransformed = {
-      id: apiData.id,
-      slug: apiData.slug,
-      title: apiData.title.rendered,
-      content: apiData.content.rendered,
-      excerpt: apiData.excerpt.rendered,
-      author_id: apiData.author,
-      date: apiData.date,
-      date_gmt: apiData.date_gmt,
-      guid: apiData.guid.rendered,
-      modified: apiData.modified,
-      modified_gmt: apiData.modified_gmt,
-      status: apiData.status,
-      type: apiData.type,
-      link: apiData.link,
-      is_protected: apiData.content.protected,
-      featured_media: apiData.featured_media,
-      parent: apiData.parent,
-      menu_order: apiData.menu_order,
-      comment_status: apiData.comment_status,
-      ping_status: apiData.ping_status,
-      template: apiData.template,
-      meta: apiData.meta,
-      acf: apiData.acf,
-      _links: apiData._links,
-
-    return apiDataTransformed
     const typedData: IWPPage = data[0]
+    const page = {
+      id: typedData.id,
+      slug: typedData.slug,
+      title: typedData.title.rendered,
+      content: typedData.content.rendered,
+      excerpt: typedData.excerpt.rendered,
+      author_id: typedData.author,
+      date: typedData.date,
+      date_gmt: typedData.date_gmt,
+      guid: typedData.guid.rendered,
+      modified: typedData.modified,
+      modified_gmt: typedData.modified_gmt,
+      status: typedData.status,
+      type: typedData.type,
+      link: typedData.link,
+      is_protected: typedData.content.protected,
+      featured_media: typedData.featured_media,
+      parent: typedData.parent,
+      menu_order: typedData.menu_order,
+      comment_status: typedData.comment_status,
+      ping_status: typedData.ping_status,
+      template: typedData.template,
+      meta: typedData.meta,
+      acf: typedData.acf,
+      _links: typedData._links,
     } as IPages
+
+    return page
+  }
   }
 
   return {
