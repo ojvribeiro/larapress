5410dec7fbfa297643437a406841985272237ec9
Victor Ribeiro <devjvribeiro@gmail.com>
2023-05-21 23:57:30 -0300

    feat: useWP().posts() util

--- app/larapress/wp/composables/useWP.ts
+++ app/larapress/wp/composables/useWP.ts
@@ -45,6 +45,53 @@ export function useWP() {
 
     return page
   }
+
+  /**
+   * Fetches all posts from the WP REST API
+   * @returns posts - an array of posts
+   * @example
+   * ```ts
+   * const posts = useWP().posts()
+   * ```
+   * @todo add pagination
+   */
+  const posts = async () => {
+    const response = await fetch(`/wp-json/wp/v2/posts`)
+    const data = await response.json()
+
+    let posts = []
+
+    data.map((post: IWPPost) => {
+      posts.push({
+        id: post.id,
+        slug: post.slug,
+        title: post.title.rendered,
+        content: post.content.rendered,
+        excerpt: post.excerpt.rendered,
+        author_id: post.author,
+        date: post.date,
+        date_gmt: post.date_gmt,
+        guid: post.guid.rendered,
+        modified: post.modified,
+        modified_gmt: post.modified_gmt,
+        status: post.status,
+        type: post.type,
+        link: post.link,
+        is_protected: post.content.protected,
+        featured_media: post.featured_media,
+        parent: post.parent,
+        menu_order: post.menu_order,
+        comment_status: post.comment_status,
+        ping_status: post.ping_status,
+        template: post.template,
+        meta: post.meta,
+        acf: post.acf,
+        _links: post._links,
+      })
+    })
+
+    return posts as IPost[]
+  }
   }
 
   return {
